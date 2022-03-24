07abcb41ee9ba5261c0f286b1f61a450afa5b05b
Jeff S.A <jeff_zb@msn.com>
2022-03-24 20:15:20 -0300

    logout criado

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,11 @@
 <?php
 
 use App\Http\Controllers\EpisodiosController;
+use App\Http\Controllers\Login\Cadastro\LoginCadastroController;
 use App\Http\Controllers\Login\LoginController;
+use App\Http\Controllers\Login\LogoutController;
 use App\Http\Controllers\SeriesController;
 use App\Http\Controllers\TemporadasController;
-use App\Models\Episodio;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -24,21 +25,37 @@ Route::get('/docLaravel', function () {
 
 //  SERIES
 Route::get('/', [SeriesController::class, "index"])
+    ->middleware('auth')
     ->name("listar_series");    // index
 Route::get("/serie/adicionar", [SeriesController::class, 'create'])
+    ->middleware('auth')
     ->name("form_criar_serie"); // form create
-Route::post("/serie/adicionar", [SeriesController::class, 'store']); // salvar form create
-Route::delete("/serie/{id_serie}", [SeriesController::class, 'destroy']); // deletar serie
-Route::post('/serie/{id}/editarNome', [SeriesController::class, 'editarNome']);
+Route::post("/serie/adicionar", [SeriesController::class, 'store'])
+    ->middleware('auth'); // salvar form create
+Route::delete("/serie/{id_serie}", [SeriesController::class, 'destroy'])
+    ->middleware('auth'); // deletar serie
+Route::post('/serie/{id}/editarNome', [SeriesController::class, 'editarNome'])
+    ->middleware('auth');
 
 //  TEMPORADAS
-Route::get('/serie/{serieId}/temporadas', [TemporadasController::class, 'index']);
+Route::get('/serie/{serieId}/temporadas', [TemporadasController::class, 'index'])
+    ->middleware('auth');
 
 //  EPISODIOS
-Route::get('/temporada/{temporada}/episodios', [EpisodiosController::class, 'index']);
-Route::post('/temporada/{temporada}/episodios/assistir', [EpisodiosController::class, 'assistir']);
+Route::get('/temporada/{temporada}/episodios', [EpisodiosController::class, 'index'])
+    ->middleware('auth');
+Route::post('/temporada/{temporada}/episodios/assistir', [EpisodiosController::class, 'assistir'])
+    ->middleware('auth');
 
 //  LOGIN
 Route::get('/login', [LoginController::class, 'index'])
     ->name('login');
 Route::post('/login', [LoginController::class, 'fazerLogin']);
+
+//  LOGOUT
+Route::resource('logout', LogoutController::class)->only('index');
+
+//  LOGIN - CADASTRO
+Route::get('/login/cadastrar', [LoginCadastroController::class, 'index'])
+    ->name('loginCadastrar');
+Route::post('/login/cadastrar', [LoginCadastroController::class, 'store']);
