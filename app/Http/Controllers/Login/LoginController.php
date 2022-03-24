330c12cdd181e63edcb39e0cb7d45a3e54c5d675
Jeff S.A <jeff_zb@msn.com>
2022-03-24 20:14:13 -0300

    hotfix: auth attempt corrigido

--- app/Http/Controllers/Login/LoginController.php
+++ app/Http/Controllers/Login/LoginController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers\Login;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\LoginRequest;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
@@ -16,13 +15,17 @@ class LoginController extends Controller
 
     public function fazerLogin(LoginRequest $request)
     {
-        if(! Auth::attempt([$request->only(['email', 'password'])]) )
+
+        $email = $request->email;
+        $password = $request->password;
+        
+        if(! Auth::attempt(['email' => $email, 'password' => $password]) )
         {
             return redirect()
                 ->back()
                 ->withErrors('E-mail e/ou senha estão incorretos.');
         }
 
-        return redirect()->route('lista_series');
+        return redirect()->route('listar_series');
     }
 }
