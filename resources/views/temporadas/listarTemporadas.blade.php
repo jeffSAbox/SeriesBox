bf71db8d6fcf2d26a0ea73f5e3d923c5ec8b2629
Jeff S.A <jeff_zb@msn.com>
2022-04-06 17:43:19 -0300

    verificar se capa existe na pagina temporadas

--- resources/views/temporadas/listarTemporadas.blade.php
+++ resources/views/temporadas/listarTemporadas.blade.php
@@ -7,11 +7,14 @@
 @section('conteudo')
 
 <ul class="list-group">
+    @if( $serie->capa )
     <div class="row">
         <div class="col-12 text-center">
             <img src="{{ $serie->capa_url }}" height="200" width="200" class="img-fluid mb-3" alt="{{ $serie->nome }}">
         </div>
     </div>
+    @endif
+
     @foreach ($temporadas as $temporada)
     <li class="list-group-item">
         <div class="d-flex justify-content-between align-items-center">
@@ -24,6 +27,7 @@
         </div>
     </li>    
     @endforeach
+    
 </ul>
 @endsection
 
